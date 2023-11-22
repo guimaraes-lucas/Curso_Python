27ddcc0034d8e005ddf7ef617dea8406a269eddf
guimaraes-lucas <luc53706@gmail.com>
2023-11-22 17:51:45 -0300

    First Commit

---
+++ exercícios Mundo 3/ex077.py
@@ -0,0 +1,24 @@
+"""
+    Crie um programa que tenha uma TUPLA com VÁRIAS PALAVRAS (não usar acentos). Depois disso, você deve mostrar, para
+cada palavra, quais são as suas VOGAIS.
+"""
+
+palavras = ('Amor', 'Sorvete', 'Jogo', 'Paralelepipedo', 'Computador')
+
+"""
+for vogais in range(0, len(palavras)):
+
+    print(f'Na palavra {palavras[vogais]}: ')
+    print(f'A letra A aparece: {palavras[vogais].upper().count("A")} vezes;')
+    print(f'A letra E aparece: {palavras[vogais].upper().count("E")} vezes;')
+    print(f'A letra I aparece: {palavras[vogais].upper().count("I")} vezes;')
+    print(f'A letra O aparece: {palavras[vogais].upper().count("O")} vezes;')
+    print(f'A letra U aparece: {palavras[vogais].upper().count("U")} vezes')
+    print('-'*40)
+"""
+
+for vogais in palavras:
+    print(f'\nNa palavra {vogais} temos: ', end='')
+    for letra in vogais:
+        if letra.lower() in 'aeiou':
+            print(letra, end=' ')
